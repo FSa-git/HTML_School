2eed07f29e165faeeaa04fbcb697d2af4e3550c8
FS <francesco9721@gmail.com>
2023-02-14 10:52:45 +0100

    New JS

---
+++ JavaScript/01_JavaScript_Start/javascript.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+ console.log("Hello World again");
+//  console.log(document.getElementById("a1").innerHTML);
+let ausgabe = document.getElementById("a1");
+console.log(ausgabe);
+})
