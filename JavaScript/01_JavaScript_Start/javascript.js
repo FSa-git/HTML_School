7bacd94189dc966bc52ca10d0acd60958a73a503
FS <francesco9721@gmail.com>
2023-02-14 11:06:17 +0100

    New

--- JavaScript/01_JavaScript_Start/javascript.js
+++ JavaScript/01_JavaScript_Start/javascript.js
@@ -3,4 +3,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
 //  console.log(document.getElementById("a1").innerHTML);
 let ausgabe = document.getElementById("a1");
 console.log(ausgabe);
+console.log(ausgabe.innerHTML);
+ausgabe.innerHTML += "<br> und das finde ich toll";
+console.log(ausgabe.innerHTML);
+
+let container = document.getElementsByTagName("div");
+console.log(container);
 })
