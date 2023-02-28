e203ed5ee2a2d7113eaeee2b14ee9c55f4b6f1af
FS <francesco9721@gmail.com>
2023-02-28 11:11:05 +0100

    Neu

--- JavaScript/01_JavaScript_Start/javascript.js
+++ JavaScript/01_JavaScript_Start/javascript.js
@@ -14,7 +14,31 @@ alert(container[0].innerHTML);
 alert(container[0].innerText); 
 
 for(let i = 0; i < container.length; i++){
-
+    ausgabe.innerHTML += "<br>Das "  + (i+1)  + "te Element hat die id " + container[i].id  
+    console.log(ausgabe.innerHTML);
 }
+let x1=document.getElementById("a2"); 
+let y1=x1.childNodes[1].childNodes[0];
+    y1.nodeValue="Versuch"; // Umändern auf "Versuch"
+
+    x1.style.fontSize="1.8em"; 
+    x1.style.color = "#b80926";
+
+/* auf das Element mit der id=b1 zugreifen*/
+let b1 = document.getElementById("b1");
+
+/* wir erstellen einen Text(knoten) mit dem Inhalt "Hier steht.." und schreiben diesen in die Variable content */
+let content = document.createTextNode("Hier steht der Inhalt von B1"); 
+
+/* Wir erstellen ein leeres p-Element */
+let paragraph = document.createElement("p"); // Ich mach ein Text dann ein p und dann tuhe ich das in b1 div rein, erst das, weil sonst entstehen iwo Lücken, wenn wenn wir ein div und dann ein Bild tun. Er erkennt die Größe nicht
+
+/*der content wird dem Element paragraph als Kind hinzugefügt*/
+paragraph.appendChild(content);
+
+/* der Paragraph (mit Text) wird als Kind dem Element mit der Id b1 hinzugefügt */
+b1.appendChild(paragraph);
+
+
 
 })
