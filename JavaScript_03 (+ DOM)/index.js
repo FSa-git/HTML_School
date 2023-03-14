da69466cdfd020add2e7c650ab920210d391a094
FS <francesco9721@gmail.com>
2023-03-14 10:34:22 +0100

    New

---
+++ JavaScript_03 (+ DOM)/index.js
@@ -0,0 +1,30 @@
+/* Hier können die JS Anweisungen formuliert werden*/
+
+let hoch = document.getElementById("up");
+let runter = document.getElementById("down")
+
+hoch.addEventListener("click", () => {
+
+    // Originalelement zaehler holen und in Variable schreiben
+    zaehlerausgabe = document.getElementById("zaehler");
+
+    // console.log(zaehlerausgabe)
+    // Text entnehmen und parsen
+    let zahl = parseInt(zaehlerausgabe.innerText);
+
+    // zurückschreiben
+    zaehlerausgabe.innerText = zahl + 1;
+
+    // Erweiterung: Zugriff auf die zwei "Dinge", auf die ich zugreifen kann. 
+    if(zahl + 1 >= 10){
+        // HTML Objekt Zugriff
+    }
+
+    console.log(zahl)
+
+    document.getElementById("up").innerHTML
+    // Text entnehmen und als Zahl parsen
+                     = parseInt
+                    
+                    //zurückschreiben
+    });
