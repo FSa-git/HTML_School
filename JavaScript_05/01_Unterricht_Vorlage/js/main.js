7d86c95fbb4c7a0e0bc93350238d60b0c953c318
FS <francesco9721@gmail.com>
2023-04-04 10:42:53 +0200

    Aufgabe weitergemacht

--- JavaScript_05/01_Unterricht_Vorlage/js/main.js
+++ JavaScript_05/01_Unterricht_Vorlage/js/main.js
@@ -7,19 +7,34 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(welcomeHere)
     console.log(welcomeHere.innerText)
 
+    // Version 1
     welcomeHere.addEventListener("click", welcome)
-
     function welcome() {
         welcomeHere.innerHTML = "Willkommen <strong>Klasse I13</strong>";
     }
 
     // Zum Vergleich:
 
+    // Version 2
     /* welcomeHere.addEventListener("click", () => {
      welcomeHere.innerHTML = "Willkommen <strong>Klasse I13!!!!!</strong>";
      })*/
 
     let Headings = document.getElementsByClassName("ueberschriften")
      // Aufgabe: die Texte unter den Überschriften sollen als erstes ausgeblendet werden (progressive Enhancement)
+
+     // Vergleich 
+    //  let Ueberschrift = document.querySelectorAll("ueberschriften")
+    //  console.log(Ueberschrift)
+
+    for (const Heading of Headings){
+        // console.log(Heading)
+        let inhaltParagraph = Heading.nextElementSibling; // wir nehmen alle, greifen auf das nachvollgende Kind zu und geben dem eine Variable zugegeben 
+        inhaltParagraph.classList.add("invisible"); // es wird hier die Klasse invisible zu einer Klasse hinzugefügen
+
+        Heading.addEventListener("click", ()=> {
+            inhaltParagraph.classList.toggle("invisible");
+        })   
+    }
 
 })
