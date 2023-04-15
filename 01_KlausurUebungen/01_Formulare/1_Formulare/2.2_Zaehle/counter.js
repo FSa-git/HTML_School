59e99449477b783be5913b5ea32e9dabf8a382f4
FS <francesco9721@gmail.com>
2023-04-15 17:51:04 +0200

    new try

---
+++ 01_KlausurUebungen/01_Formulare/1_Formulare/2.2_Zaehle/counter.js
@@ -0,0 +1,20 @@
+let hoch = document.getElementById("up")
+let runter = document.getElementById("down")
+
+hoch.addEventListener("click", ()=> {
+    let counterOutPut = document.getElementById("counter");
+
+    let numbr = parseInt(counterOutPut.innerText);
+
+    counterOutPut.innerText = numbr + 1;
+});
+
+runter.addEventListener("click", ()=> {
+
+counterOutPut = document.getElementById("zaehler");
+
+let numbr = parseInt(counterOutPut.innerText);
+
+counterOutPut.innerText = numbr - 2;
+
+});
