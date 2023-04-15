10f1fcfc82b977a61dcb79c2e012ab23d213b0fc
FS <francesco9721@gmail.com>
2023-04-15 18:24:10 +0200

    add

--- 01_KlausurUebungen/01_Formulare/1_Formulare/Yt_Tutorial/app.js
+++ 01_KlausurUebungen/01_Formulare/1_Formulare/Yt_Tutorial/app.js
@@ -1,1 +1,23 @@
-console.log("Hallo Welt");
+var playerName = 'Max'; // deklariert und initialisiert
+var highscore = 100; // Datentyp number
+var gameOver = false; 
+var test; // Datentyp undefined 
+var nüscht = null; // Datentyp object
+
+let secondPlayer = 'Jan';
+
+if(true){
+    var playerName = "Mario"
+}
+
+console.log(typeof gameOver); // sagt was für ein Datentyp das ist  
+
+console.log(playerName);
+
+// Funktionen 
+
+function calculate(number1, number2){
+    console.log(number1 + number2);
+}
+
+calculate(); // Jetzt wird es ausgegeben
