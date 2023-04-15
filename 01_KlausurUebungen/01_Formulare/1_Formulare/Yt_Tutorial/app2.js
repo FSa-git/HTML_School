162fa2312bb64b8f4b1b864e6fc06db4a47e530c
FS <francesco9721@gmail.com>
2023-04-15 18:44:44 +0200

    added

---
+++ 01_KlausurUebungen/01_Formulare/1_Formulare/Yt_Tutorial/app2.js
@@ -0,0 +1,12 @@
+let highscore = '100';
+
+if(highscore > 90) {
+    console.log('Du bist gut!')
+}
+
+// Bei einem Vergleich wird bei 3=== auch der Datentyp verglichen 
+// Bei 2== nur Vergleich 
+
+if(highscore == 100){
+    console.log('Du bist 100')
+}
