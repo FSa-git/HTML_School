ca6df25f9b5ad91ba0971465fb35194293176b6c
FS <francesco9721@gmail.com>
2023-04-16 16:13:46 +0200

    new

---
+++ 01_KlausurUebungen/4_JS_Smiley/index.js
@@ -0,0 +1,17 @@
+let happy = document.getElementById('happy_face');
+// let sad = document.getElementById('sad_face');
+
+happy.addEventListener('click', ()=>{
+
+    // document.getElementById('smiley').src = '/JavaScript_03 (+ DOM)/Smil_img/smiley1.png';
+    document.getElementById('smiley').src = "/JavaScript_03 (+ DOM)/Smil_img/smiley2.png"
+
+    happy.disable
+
+});
+
+// sad.addEventListener('click', ()=>{
+
+//     document.getElementById('smiley').src = "/JavaScript_03 (+ DOM)/Smil_img/smiley2.png"
+
+// });
