8baece469770dca602b7bfa426474fbf9dc014b2
FS <francesco9721@gmail.com>
2023-04-23 11:00:01 +0200

    new exercise answered

--- Ordner_JS_Gruppen/Gruppe5_1_infoString-Array.js
+++ Ordner_JS_Gruppen/Gruppe5_1_infoString-Array.js
@@ -10,3 +10,6 @@ let shoppingList = ['Apfel', 'Banane', 'Pfirsich']
 console.log(shoppingList.join(", "))
 
 //Überprüfen Sie den Typ jeweils vor und nach der Umwandlung mit typeof
+
+console.log('Typ von Shopping ist: ' + typeof shopping);
+console.log('Typ von ShoppingList ist: ' + typeof shoppingList);
