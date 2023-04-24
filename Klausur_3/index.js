1c8712e9dab9f4a58e6449b0e90a76ae6352da84
FS <francesco9721@gmail.com>
2023-04-24 18:29:27 +0200

    Vollständige Aufgabe

--- Klausur_3/index.js
+++ Klausur_3/index.js
@@ -1,34 +1,36 @@
-const form = document.querySelector('form');
-const firstNameInput = document.querySelector('#first_name');
-const lastNameInput = document.querySelector('#second_name');
-const phoneInput = document.querySelector('#userNr');
-const emailInput = document.querySelector('#userAddress');
-const franceCheckbox = document.querySelector('input[name="france"]');
-const italyCheckbox = document.querySelector('input[name="ita"]');
-const austriaCheckbox = document.querySelector('#oesi');
+// Warten, bis das Dokument vollständig geladen ist
+document.addEventListener('DOMContentLoaded', function() {
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const firstName = firstNameInput.value;
-  const lastName = lastNameInput.value;
-  const phone = phoneInput.value;
-  const email = emailInput.value;
-  const destinations = [];
-
-  if (franceCheckbox.checked) {
-    destinations.push('Frankreich');
-  }
-
-  if (italyCheckbox.checked) {
-    destinations.push('Italien');
-  }
-
-  if (austriaCheckbox.checked) {
-    destinations.push('Österreich');
-  }
-
-  const message = `Vielen Dank, ${firstName} ${lastName}, für Ihre Anfrage! Wir haben Ihre Kontaktdaten ${phone} und ${email} gespeichert und werden uns bald bei Ihnen melden. Sie haben folgende Reiseziele ausgewählt: ${destinations.join(', ')}.`;
-
-  alert(message);
-});
+    // Formular abrufen
+    const form = document.querySelector('form');
+  
+    // Ereignis-Listener für das Absenden des Formulars hinzufügen
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Verhindert das tatsächliche Absenden des Formulars
+  
+      // Eingabefelder abrufen
+      const firstName = document.querySelector('#first_name').value;
+      const lastName = document.querySelector('#second_name').value;
+      const phoneNum = document.querySelector('#userNr').value;
+      const email = document.querySelector('#userAddress').value;
+      const france = document.querySelector('input[name="france"]').checked;
+      const italy = document.querySelector('input[name="ita"]').checked;
+      const austria = document.querySelector('#oesi').checked;
+  
+      // Erstelle Text für die Bestätigungsnachricht
+      let message = `Vielen Dank für Ihre Anfrage, ${firstName} ${lastName}!`;
+      message += `\nWir haben Ihnen eine Bestätigung an ${email} gesendet.`;
+      message += `\n\nFolgende Reiseziele wurden ausgewählt:`;
+      if (france) message += '\n- Frankreich';
+      if (italy) message += '\n- Italien';
+      if (austria) message += '\n- Österreich';
+  
+      // Erstelle ein neues Element für die Bestätigungsnachricht
+      const confirmation = document.createElement('p');
+      confirmation.textContent = message;
+  
+      // Füge das Element unterhalb des Formulars ein
+      const parent = document.querySelector('#parent');
+      parent.appendChild(confirmation);
+    });
+  });
