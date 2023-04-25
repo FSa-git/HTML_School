584c21a75517c4f6efe363654f997375c51c8785
FS <francesco9721@gmail.com>
2023-04-25 10:55:46 +0200

    new fade-in

--- JavaScript_07 SlideShow/vorlage_slideshow/js/main.js
+++ JavaScript_07 SlideShow/vorlage_slideshow/js/main.js
@@ -9,8 +9,22 @@ const imgs = document.querySelectorAll('.imgs img')
 
 const opacity = 0.4
 
+// NEU!!
+let currentPicture = imgs[0];
+
+//opacity für das erste Bild setzen
+// imgs[0].style.opacity = opacity;
+
 // Bildtransparenz / set opacity
 imgs[0].getElementsByClassName.opacity = opacity;
+
+// fadeIn function NEU!!
+function fadeIn(x) {
+    // Add fading class
+    x.classList.add('fade-in');
+    // remove fade-in class after .5 seconds
+    setTimeout(() => x.classList.remove('fade-in'), 500);
+}
 
 // EventListener für alle Bilder setzen 
 imgs.forEach(imgs => imgs.addEventListener('click', imgsClick))
@@ -23,6 +37,9 @@ for(let i = 0; i < imgs.length; i++){
 }*/
 
 // e steht für Element
+
+
+
 function imgsClick(e){
     
     // Opacity zurücksetzen 
@@ -31,6 +48,7 @@ function imgsClick(e){
     //Bild austauschen auf das angeklickte Bild
     current.src = e.target.src;
 
+    currentPicture = e.target;
     //Fading Class hinzufügen
     current.classList.add('fade-in')
 
@@ -40,5 +58,43 @@ function imgsClick(e){
     //Opacity für ausgewähltes Bild setzen
     e.target.style.opacity = opacity;
 }
+
+// Jetzt für die Forwards-Button 
+const forward = document.getElementById('forward');
+    forward.addEventListener('click', forwardFunction);
+    function forwardFunction() {
+        //Durchsichtigkeit zurücksetzen
+        currentPicture.style.opacity = 1;
+
+        // wenn es ein nächstes Element gibt - als Bild nehmen, wenn nicht über Elternelement das erste Kind auswählen
+        
+        if (currentPicture.nextElementSibling) {
+            currentPicture = currentPicture.nextElementSibling;
+        } 
+        else {
+            currentPicture = currentPicture.parentElement.firstElementChild;
+        }
+    current.src = currentPicture.src;
+    fadeIn(current);
+    currentPicture.style.opacity = opacity;
+    }
+
+    // Zurück Button 
+
+    const back = document.getElementById('back');
+
+    back.addEventListener('click', backFunction);
+
+    function backFunction() {
+        currentPicture.style.opacity = 1
+        if (currentPicture.previousElementSibling) {
+            currentPicture = currentPicture.previousElementSibling;
+        } else {
+            currentPicture = currentPicture.parentElement.lastElementChild;
+        }
+        current.src = currentPicture.src;
+        fadeIn(current);
+        currentPicture.style.opacity = opacity;
+    }
 
 });
