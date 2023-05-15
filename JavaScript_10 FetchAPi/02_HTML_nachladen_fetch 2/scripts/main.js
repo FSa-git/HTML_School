8ba2750e099fef13a67421087d33db5d534f7b6f
FS <francesco9721@gmail.com>
2023-05-15 11:50:59 +0200

    todays lesson

---
+++ JavaScript_10 FetchAPi/02_HTML_nachladen_fetch 2/scripts/main.js
@@ -0,0 +1,37 @@
+'use strict';
+function init() {
+  console.log("Daten mit Fetch API laden");
+  let login = document.getElementById('login');
+  let register = document.getElementById('register');
+  login.addEventListener('click', (e) => {
+    /* Kommentar ergänzen: Was macht preventDefault? */
+    e.preventDefault();
+    loadContent('login');
+  });
+  register.addEventListener('click', (e) => {
+    e.preventDefault();
+    loadContent('registerin');
+  });
+}
+
+function loadContent(name) {
+/* Kommentar ergänzen: Was sind async und await?  */
+  async function fetchContent() {
+    let response = await fetch(name + '.html');
+    console.log(response.status); // 200
+    console.log(response.statusText); // OK
+    if (response.status === 200) {
+      let htmlSnippet = await response.text();
+      /*console.log(htmlSnippet);*/
+      document.getElementById('main-content').innerHTML = htmlSnippet;
+
+    } else {
+      /* Füge eine Warnmeldung hinzu */
+      
+    }
+  }
+  fetchContent();
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
